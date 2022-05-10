97e21bf9999f452146b3ffda2e2619e106d0dd7b
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-05-10 21:35:22 +0800

    Created Navbar and Error component

---
+++ src/pages/homepage.jsx
@@ -0,0 +1,10 @@
+import NavigationBar from "../components/navbar";
+
+export default function HomePage() {
+    return (
+        <div>
+            <NavigationBar />
+            <h1>Test</h1>
+        </div>
+    )
+}
