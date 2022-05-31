39585f11f228a590f9047e18bdc459e8e6f5181e
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-06-01 07:51:42 +0800

    Updated the routes in react router

--- website/client/src/App.js
+++ website/client/src/App.js
@@ -1,6 +1,5 @@
 //? First container gives a min. height of 100% and sets a fixed height of the container to 100vh. Afterwards, it centers everything via flex and justify center to center them horizontally.
 
-import { BrowserRouter } from "react-router-dom";
 import * as React from "react";
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/authentication/login";
@@ -9,15 +8,13 @@ import Error from "./pages/error/error";
 import HomePage from "./pages/public/homepage";
 import Dashboard from "./pages/private/dashboard";
 import EventPage from "./pages/public/events";
-import UpdateEventPage from "./pages/authentication/eventUpdate";
+import UpdateEventPage from "./pages/private/eventUpdate";
 import EventDetailsPage from "./pages/public/eventDetails";
 import EventForm from "./components/forms/event";
 
 export default function App() {
   return (
-    // <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div className="w-full space-y-8">
-      <BrowserRouter>
         <Routes>
           <Route path="*" element={<Error />} />
           <Route path="/" element={<HomePage />} />
@@ -32,8 +29,6 @@ export default function App() {
           <Route path="event/update/:id" element={<UpdateEventPage />} />
           <Route path="event/:id" element={<EventDetailsPage />} />
         </Routes>
-      </BrowserRouter>
     </div>
-    // </div>
   );
 }
