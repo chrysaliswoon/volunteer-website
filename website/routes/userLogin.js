d80bc0a3b52db4c5b0267021ff3245ae788e70e9
Chrysalis <63181733+chrysaliswoon@users.noreply.github.com>
2022-06-03 19:13:05 +0800

    Fixed backend login credentials

--- website/routes/userLogin.js
+++ website/routes/userLogin.js
@@ -7,40 +7,68 @@ const express = require("express");
 const login = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const createError = require("http-errors");
 
 //? LOGIN Route
 
 login.post("/", async (req, res) => {
-  const data = req.body;
-  // Read email from request body
-  const { email, password } = data;
-
-  // Find user from the users array by email
-  const user = await prisma.volunteer.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (!user) {
-    res.send("User not registered");
+  try {
+    const authUser = await prisma.volunteer.findUnique({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (!authUser) {
+      res.send({msg: "User not found"});
+    } else {
+      if (bcrypt.compareSync(req.body.password, authUser.password)) {
+        const accessToken = jwt.sign(
+            { authUsername: authUser.authUsername, email: authUser.email, id: authUser.id },
+            accessTokenSecret
+          );
+          res.cookie("jwt_token", accessToken, {
+            expires: new Date(Date.now() + 16 * 3600000),
+            path: "/",
+            httpOnly: true,
+          });
+          res.status(200).send({ msg: "Logged in successfully!", accessToken });
+      } else {
+        res.send({ msg: "Invalid login credentials" });
+        delete authUser.password;
+      }
+    }
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
   }
 
-  const checkPassword = bcrypt.compareSync(password, user.password);
-  if (!checkPassword) res.send("Invalid Email address or password");
+  // const data = req.body;
+  // const { email, password } = data;   // Read email from request body
 
-  delete user.password;
+  // const authUser = await prisma.volunteer.findUnique({   // Find authUser from the authUsers array by email
+  //   where: {
+  //     email: req.body.email,
+  //   },
+  // });
 
-  const accessToken = jwt.sign(
-    { username: user.username, email: user.email, id: user.id },
-    accessTokenSecret
-  );
-  res.cookie("jwt_token", accessToken, {
-    expires: new Date(Date.now() + 16 * 3600000),
-    path: "/",
-    httpOnly: true,
-  });
-  res.status(200).send({ msg: "Logged in successfully!", accessToken });
+  // if (!authUser) {
+  //   throw createError.NotFound('User not registered')
+  // }
+
+  // const checkPassword = bcrypt.compareSync(password, authUser.password);
+  // if (!checkPassword) throw createError.Unauthorized('Email address or password not valid')
+
+  // delete authUser.password;
+
+  // const accessToken = jwt.sign(
+  //   { authUsername: authUser.authUsername, email: authUser.email, id: authUser.id },
+  //   accessTokenSecret
+  // );
+  // res.cookie("jwt_token", accessToken, {
+  //   expires: new Date(Date.now() + 16 * 3600000),
+  //   path: "/",
+  //   httpOnly: true,
+  // });
+  // res.status(200).send({ msg: "Logged in successfully!", accessToken });
 });
 
 module.exports = login;
--- website/routes/userRegister.js
+++ website/routes/userRegister.js
@@ -8,20 +8,6 @@ const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
 //? REGISTER Routes
-
-// register.post("/", async (req, res) => {
-//   const data = req.body;
-//   const { name, email } = data;
-//   data.password = bcrypt.hashSync(data.password, saltRounds);
-
-//   const newUser = await prisma.volunteer.create({
-//     data,
-//   });
-//   delete newUser.password;
-//   res.json(newUser);
-//   // prisma.$disconnect();
-// });
-
 register.post("/", async (req, res) => {
   req.body.password = bcrypt.hashSync(req.body.password, 10);
   try {
@@ -53,18 +39,5 @@ register.post("/", async (req, res) => {
     res.status(400).send({ msg: error.message });
   }
 });
-
-// register.post("/", async (req, res) => {
-//   req.body.password = bcrypt.hashSync(req.body.password, saltRounds);
-
-//   const user = await prisma.volunteer.create({
-//     data: {
-//       name: req.body.name,
-//       email: req.body.email,
-//       password: req.body.password
-//     },
-//   });
-//   res.json(user);
-// })
 
 module.exports = register;
